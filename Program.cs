e394dbd87adaaec4cf69b8004350c1cf1bbcc090
murgendrag <murgendra67@gmail.com>
2022-03-29 19:34:18 +0530

    program file

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace MoodAnalzerProblem
+{
+    public class Program
+    {
+         static void Main(string[] args)
+        {
+            string UserMessage;
+            bool result;
+            MoodAnalser Check = new MoodAnalser();
+
+            Console.WriteLine("If sad then enter I am in Sad Mood ,If Happy then I am in Happy Mood");
+
+            UserMessage = Console.ReadLine();
+
+            result = Check.AnalyseMood(UserMessage);
+            
+
+            if (result = true)
+            {
+                Console.WriteLine("SAD");
+            }
+            else
+            {
+                Console.WriteLine("HAPPY");
+            }
+        }
+    }
+}
